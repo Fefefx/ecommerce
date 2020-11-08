5ebfb9898bac65455cc9a40fbcd608bb8eb44270
= <=>
2020-11-08 07:33:41 -0300

    Criação da classe de template

--- index.php
+++ index.php
@@ -1,17 +1,20 @@
 <?php 
 
 require_once("vendor/autoload.php");
+require_once("vendor/hcodebr/php-classes/src/page.php");
 
-$app = new \Slim\Slim();
+use Hcode\Page;
+use \Slim\Slim;
+
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
-	$sql = new \Hcode\DB\Sql();
-	$results = $sql->select("SELECT * FROM tb_users");
-	echo json_encode($results);
+	$page = new Page();
+	$page->setTpl("index");
 });
 
 $app->run();
 
- ?>
+?>
