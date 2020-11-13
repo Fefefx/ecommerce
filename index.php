a445fd4cf0ad5a88939f3d7675463c4674554147
Fefefx <felipemantoan@hotmail.com>
2020-11-13 14:30:24 -0300

    Menu de categorias na interface do site

--- index.php
+++ index.php
@@ -207,4 +207,14 @@ $app->get('/admin/categories', function(){
 	]);
 });
 
+$app->get('/categories/:idcategory', function($idcategory){
+	$category = new Category();
+	$category->get((int)$idcategory);
+	$page = new Page();
+	$page->setTpl("category",[
+		'category' => $category->getValues(),
+		'products' => []
+	]);
+});
+
 $app->run();
